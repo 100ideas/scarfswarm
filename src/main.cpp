a2c3cd99879486df4a8a05a8b49872077585ccd9
Mac Cowell <mac@has100ideas.com>
2023-05-18 22:32:03 -0700

    it works (don't understand GPIO number converntion)

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,128 @@
-#include <Arduino.h>
-#include <FastLEDHub.h>
-#include <SPI.h>
-#include "Animations/Spectrogram.h"
+// #include <FastLED.h>
 
-// https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
-#define VSPI_MISO   19
-#define VSPI_MOSI   23  // apa102 logic input ("Master Out Slave In")
-#define VSPI_SCLK   18  // apa102 clock input
-#define VSPI_SS     5
-// #define FASTLED_ALL_PINS_HARDWARE_SPI
-// #define FASTLED_ESP32_SPI_BUS VSPI
+// /// @file    DemoReel100.ino
+// /// @brief   FastLED "100 lines of code" demo reel, showing off some effects
+// /// @example DemoReel100.ino
+// /// https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
 
-#define NUM_LEDS 100
-// #define LIGHTSTRIP_PIN 5
-// #define LED_TYPE WS2812B
-#define LED_TYPE DOTSTAR
-// #define DATAPIN 13
-// #define CLOCKPIN 14
 
+// // https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
+// #define VSPI_MISO   19
+// #define VSPI_MOSI   23  // apa102 logic input ("Master Out Slave In")
+// #define VSPI_SCLK   18  // apa102 clock input
+// #define VSPI_SS     5
+// // #define FASTLED_ALL_PINS_HARDWARE_SPI
+// // #define FASTLED_ESP32_SPI_BUS VSPI
+
+// #define NUM_LEDS 100
+// // #define LIGHTSTRIP_PIN 5
+// // #define LED_TYPE WS2812B
+// #define LED_TYPE APA102
+// #define COLOR_ORDER GRB
+// // #define DATAPIN 13
+// // #define CLOCKPIN 14
+// // CRGB leds[NUM_LEDS];
+// CRGB leds[NUM_LEDS];
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+/// @file    FirstLight.ino
+/// @brief   Animate a white dot moving along a strip of LEDs
+/// @example FirstLight.ino
+
+// Use if you want to force the software SPI subsystem to be used for some reason (generally, you don't)
+// #define FASTLED_FORCE_SOFTWARE_SPI
+// Use if you want to force non-accelerated pin access (hint: you really don't, it breaks lots of things)
+// #define FASTLED_FORCE_SOFTWARE_SPI
+// #define FASTLED_FORCE_SOFTWARE_PINS
+#include <FastLED.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// Move a white dot along the strip of leds.  This program simply shows how to configure the leds,
+// and then how to turn a single pixel white and then off, moving down the line of pixels.
+// 
+
+// How many leds are in the strip?
+#define NUM_LEDS 60
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+// #define DATA_PIN 3
+// #define CLOCK_PIN 13
+#define DATA_PIN 18
+#define CLOCK_PIN 5
+
+// This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 
-void setup()
-{
-  FastLEDHub.initialize("Spectrogram Example");
-  // FastLEDHub.addLeds<LED_TYPE, LIGHTSTRIP_PIN, GRB>(leds, NUM_LEDS);
-  FastLEDHub.addLeds<LED_TYPE, VSPI_MOSI, VSPI_SCLK, BGR>(leds, NUM_LEDS);
+// This function sets up the ledsand tells the controller about them
+void setup() {
+	// sanity check delay - allows reprogramming if accidently blowing power w/leds
+   	delay(2000);
 
-  FastLEDHub.registerAnimation(new Spectrogram("Spectrogram"));
+    // Uncomment/edit one of the following lines for your leds arrangement.
+    // ## Clockless types ##
+    // FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+    // FastLED.addLeds<SM16703, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<TM1829, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<TM1812, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<UCS1904, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<UCS2903, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // FastLED.addLeds<WS2852, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // FastLED.addLeds<GS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<SK6812, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // FastLED.addLeds<SK6822, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<APA106, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<PL9823, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<SK6822, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<WS2813, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<WS2811_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<GE8822, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<LPD1886, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<LPD1886_8BIT, DATA_PIN, RGB>(leds, NUM_LEDS);
+    // ## Clocked (SPI) types ##
+    // FastLED.addLeds<LPD6803, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<WS2803, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+    // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+    // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+    FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+    // FastLED.addLeds<SK9822, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
 }
 
-void loop()
-{
-  FastLEDHub.handle();
+// This function runs over and over, and is where you do the magic to light
+// your leds.
+void loop() {
+   // Move a single white led 
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+      // Turn our current led on to white, then show the leds
+      leds[whiteLed] = CRGB::White;
+
+      // Show the leds (only one of which is set to white, from above)
+      FastLED.show();
+
+      // Wait a little bit
+      delay(100);
+
+      // Turn our current led back to black for the next loop around
+      leds[whiteLed] = CRGB::Black;
+   }
 }
