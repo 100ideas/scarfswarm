5f46d76c2b9f6ba20980f3e12456440ffc835cc7
Mac Cowell <mac@has100ideas.com>
2023-06-22 00:02:25 -0700

    knob hello world

---
+++ src/config.h
@@ -0,0 +1,12 @@
+
+
+const static uint8_t PIN_RADIO_CE = 4;
+const static uint8_t PIN_RADIO_CSN = 5;
+const static uint8_t PIN_RADIO_MOSI = 23;
+const static uint8_t PIN_RADIO_MISO = 19;
+const static uint8_t PIN_RADIO_SCK = 18;
+const int CLOCKPIN = 14;
+const int DATAPIN = 13;
+const int buttonPin = 39;
+const uint8_t rotary1 = 34;
+const uint8_t rotary2 = 36;
