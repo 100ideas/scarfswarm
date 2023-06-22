a29b203acffb04a21f63305e0e55363c2792ff91
Mac Cowell <mac@has100ideas.com>
2023-06-22 01:47:04 -0700

    it compliles, knob positions change but unexpectedly erratic; problem

--- src/config.h
+++ src/config.h
@@ -1,6 +1,18 @@
 #pragma once
 #include <cstdint>
 
+/*
+Radio    ESP32 module
+CE    -> 4
+CSN   -> 5
+MOSI  -> 23
+MISO  -> 19
+SCK   -> 18
+IRQ   -> No connection
+VCC   -> No more than 3.6 volts
+GND   -> GND
+*/
+
 const static uint8_t PIN_RADIO_CE = 4;
 const static uint8_t PIN_RADIO_CSN = 5;
 const static uint8_t PIN_RADIO_MOSI = 23;
