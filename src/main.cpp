cfe3121fbae8b1fed14790e7f9512eaf8e3c4bbf
Mac Cowell <mac@has100ideas.com>
2023-08-03 18:29:30 -0400

    basic FastLED animation in SPI HW w/ FreeRTOS task :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,267 +1,195 @@
-/*
-https://github.com/dparson55/NRFLite/blob/4e425d742ca8879d654a270c7c02c13440476e7a/examples/Basic_RX_ESP32/Basic_RX_ESP32.ino
-
-Demonstrates simple RX operation with an ESP32.
-Any of the Basic_TX examples can be used as a transmitter.
-
-ESP's require the use of '__attribute__((packed))' on the RadioPacket data structure
-to ensure the bytes within the structure are aligned properly in memory.
-
-The ESP32 SPI library supports configurable SPI pins and NRFLite's mechanism to support this is shown.
-*/
-
-#include "SPI.h"
-#include "NRFLite.h"
-#include "config.h"
-#include "Knob.h"
 #include <Arduino.h>
+// #define ARDUINO_RUNNING_CORE 1
 
-struct __attribute__((packed)) RadioPacket // Note the packed attribute.
-{
-    uint8_t SenderId;
-    uint32_t OnTimeMillis;
-    uint32_t FailedTxCount;
-};
+/* FastLED ESP32 Hardware SPI Driver
+ * https://github.com/FastLED/FastLED/blob/master/src/platforms/esp/32/fastspi_esp32.h
+ *
+ * This hardware SPI implementation can drive clocked LEDs from either the
+ * VSPI or HSPI bus (aka SPI2 & SPI3). No support is provided for SPI1, because it is 
+ * shared among devices and the cache for data (code) in the Flash as well as the PSRAM.
+ *
+ * To enable the hardware SPI driver, add the following line *before* including
+ * FastLED.h:
+ *
+ * #define FASTLED_ALL_PINS_HARDWARE_SPI
+ *
+ * This driver uses the VSPI bus by default (GPIO 18, 19, 23, & 5). To use the 
+ * HSPI bus (GPIO 14, 12, 13, & 15) add the following line *before* including
+ * FastLED.h:
+ * 
+ * #define FASTLED_ESP32_SPI_BUS HSPI
+ * 
+ */
 
-NRFLite _radio;
-RadioPacket _radioData;
+// may not be necessary, but FastLED.h kicks off an include to led_sysdefs.h, which checks for ESP32,
+// then includes https://github.com/FastLED/FastLED/blob/master/src/led_sysdefs.h#L41
+#ifndef ESP32
+#define ESP32
+#endif
 
-const static uint8_t RADIO_ID = random();
-const static uint8_t SHARED_RADIO_ID = 42;
+#define FASTLED_ALL_PINS_HARDWARE_SPI
+#define FASTLED_ESP32_SPI_BUS VSPI
+/* https://github.com/FastLED/FastLED/blob/master/src/platforms/esp/32/fastspi_esp32.h#L58C1-L61C30
+ *  static uint8_t spiClk = 18;
+ *  static uint8_t spiMiso = 19;
+ *  static uint8_t spiMosi = 23;
+ *  static uint8_t spiCs = 5;
+ */
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////
-//
-// FastLED hw SPI pull request
-// https://github.com/FastLED/FastLED/pull/1047
-//
-// // https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
-// // https://lastminuteengineers.com/esp32-pinout-reference/
-// #define VSPI_MISO   19
-// #define VSPI_MOSI   23  // apa102 logic input ("Master Out Slave In")
-// #define VSPI_SCLK   18  // apa102 clock input
-// #define VSPI_SS     5
-// // #define FASTLED_ALL_PINS_HARDWARE_SPI
-// // #define FASTLED_ESP32_SPI_BUS VSPI
-
-// Use if you want to force the software SPI subsystem to be used for some reason (generally, you don't)
-// #define FASTLED_FORCE_SOFTWARE_SPI
-// Use if you want to force non-accelerated pin access (hint: you really don't, it breaks lots of things)
-// #define FASTLED_FORCE_SOFTWARE_SPI
-// #define FASTLED_FORCE_SOFTWARE_PINS
-
-// TODO figure out why PIO GCC is unhappy with Fx declarations out of lexical order...
-// ... possibly related to platformio.ini lib_ldf_mode = deep 
-
+// #TODO want to find a clean way to pull these defs from where they are already defined in fastspi_esp32.h
+#define LED_spiClk 18
+#define LED_spiMosi 23
 #include <FastLED.h>
 
-// #define DATA_PIN 18
-// #define CLOCK_PIN 5
-#define DATA_PIN 13
-#define CLOCK_PIN 14
 #define NUM_LEDS 60
 #define FRAMES_PER_SECOND 60
-
-bool gReverseDirection = false;
-
-// This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 
-// Knob * knob{};
-Knob knob{};
+void vMainTaskAnimCylon( void *pvParameters );
+void vMainTaskAnalogRead( void *pvParameters );
+TaskHandle_t analog_read_task_handle; // You can (don't have to) use this to be able to manipulate a task from somewhere else.
+#define ANALOG_INPUT_PIN 39 // Vbatt (A3 on feather)
 
-// This function sets up the ledsand tells the controller about them
+
 void setup() {
-    // knob values latching -> 0 or 100; not sure why,
-    // maybe need pulllups?
-    pinMode(rotary1, INPUT_PULLUP);
-    pinMode(rotary2, INPUT_PULLUP);
-    // TODO: getting erratic knob position counts from getCount()
-    // - review docs
-    // - set up knob in isolated bread board & esp
-    // - repliate/resolve bug
+  // Initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
 
+  // https://github.com/FastLED/FastLED/blob/master/src/FastLED.h#L246
+  FastLED.addLeds<APA102, LED_spiMosi, LED_spiClk, BGR>(leds, NUM_LEDS);  // BGR ordering is typical
+  FastLED.setBrightness(84);
+  Serial.println("main.setup(): FastLED.addLeds() complete\n");
 
-    Serial.begin(115200);
-    Serial.println("setup()...");
+  // Set up two tasks to run independently.
+  xTaskCreatePinnedToCore(
+    vMainTaskAnimCylon
+    ,  "Task AnimationCylon" // A name just for humans
+    // ,  2048        // The stack size can be checked by calling `uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);`
+    ,  4096
+    // ,  (void*) &leds // Task parameter which can modify the task behavior. This must be passed as pointer to void.
+    ,  NULL
+    ,  2  // Priority
+    ,  NULL // Task handle is not used here - simply pass NULL
+    ,  ARDUINO_RUNNING_CORE // core for task
+    );
 
-    // Configure SPI pins.
-    SPI.begin(PIN_RADIO_SCK, PIN_RADIO_MISO, PIN_RADIO_MOSI, PIN_RADIO_CSN);
-    
-    // Indicate to NRFLite that it should not call SPI.begin() during initialization since it has already been done.
-    uint8_t callSpiBegin = 0;
-    
-    // init radio w common ID so all are in "broadcast" mode
-    if (!_radio.init(SHARED_RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100, callSpiBegin))
-    {
-        Serial.println("Cannot communicate with radio");
-        // while (1); // Wait here forever.
-    }
-    Serial.println("main.setup(): _radio.init() complete");
+  // This variant of task creation can also specify on which core it will be run (only relevant for multi-core ESPs)
+  xTaskCreate(
+    vMainTaskAnalogRead
+    ,  "Analog Read"
+    ,  2048  // Stack size
+    ,  NULL  // When no parameter is used, simply pass NULL
+    ,  1  // Priority
+    ,  &analog_read_task_handle // With task handle we will be able to manipulate with this task.
+    );
 
-    _radioData.SenderId = RADIO_ID;
-    _radioData.FailedTxCount = 0;
-
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-   	delay(2000);
-
-    FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);  // BGR ordering is typical
-    Serial.println("main.setup(): FastLED.addLeds() complete\n");
-
-    knob.setup();
-    Serial.println("main.setup(): knob.setup() complete\n");
-    Serial.println("knob.get(): ");
-    Serial.print(knob.get());
+  Serial.printf("Basic Multi Threading Arduino Example\n");
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-// Fire2012 by Mark Kriegsman, July 2012
-// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
-//// 
-// This basic one-dimensional 'fire' simulation works roughly as follows:
-// There's a underlying array of 'heat' cells, that model the temperature
-// at each point along the line.  Every cycle through the simulation, 
-// four steps are performed:
-//  1) All cells cool down a little bit, losing heat to the air
-//  2) The heat from each cell drifts 'up' and diffuses a little
-//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
-//  4) The heat from each cell is rendered as a color into the leds array
-//     The heat-to-color mapping uses a black-body radiation approximation.
-//
-// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
-//
-// This simulation scales it self a bit depending on NUM_LEDS; it should look
-// "OK" on anywhere from 20 to 100 LEDs without too much tweaking. 
-//
-// I recommend running this simulation at anywhere from 30-100 frames per second,
-// meaning an interframe delay of about 10-35 milliseconds.
-//
-// Looks best on a high-density LED setup (60+ pixels/meter).
-//
-//
-// There are two main parameters you can play with to control the look and
-// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
-// in step 3 above).
-//
-// COOLING: How much does the air cool as it rises?
-// Less cooling = taller flames.  More cooling = shorter flames.
-// Default 50, suggested range 20-100 
-#define COOLING  55
-
-// SPARKING: What chance (out of 255) is there that a new spark will be lit?
-// Higher chance = more roaring fire.  Lower chance = more flickery fire.
-// Default 120, suggested range 50-200.
-#define SPARKING 120
+// cheesy cylon example - delay() is bad probably
+void fadeall() { 
+  for(int i = 0; i < NUM_LEDS; i++) { 
+    leds[i].nscale8(250); 
+  }
+  vTaskDelay(20 / portTICK_PERIOD_MS);
+}
 
 
-void Fire2012()
-{
-// Array of temperature readings at each simulation cell
-  static uint8_t heat[NUM_LEDS];
+void loop() {
+  if(analog_read_task_handle != NULL){ // Make sure that the task actually exists
+    delay(10000);
+    vTaskDelete(analog_read_task_handle); // Delete task
+    analog_read_task_handle = NULL; // prevent calling vTaskDelete on non-existing task
+    Serial.println("vTaskDelete(analog_read_task_handle); // Delete task");
+  }
+}
 
-  // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+
+void vMainTaskAnimCylon(void *pvParameters){
+  /* broken
+  CRGB leds = *((CRGB*)pvParameters);
+  */
+
+  int count = 0;
+  int uxHighWaterMark;
+  static uint8_t hue = 0;
+
+  for (;;){
+    // First slide the led in one direction
+    for(int i = 0; i < NUM_LEDS; i++) {
+      // Set the i'th led to red 
+      leds[i] = CHSV(hue++, 255, 255);
+      // Show the leds
+      FastLED.show(); 
+      // now that we've shown the leds, reset the i'th led to black
+      leds[i] = CRGB::Black;
+      fadeall();
+      
+      // arduino-esp32 has FreeRTOS configured to have a tick-rate of 1000Hz and portTICK_PERIOD_MS
+      // refers to how many milliseconds the period between each ticks is, ie. 1ms.
+      vTaskDelay(10 / portTICK_PERIOD_MS);
     }
+    Serial.print("x");
+
+    // Now go in the other direction.  
+    for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+    	// Set the i'th led to red 
+    	leds[i] = CHSV(hue++, 255, 255);
+    	// Show the leds
+    	FastLED.show();
+    	// now that we've shown the leds, reset the i'th led to black
+    	leds[i] = CRGB::Black;
+    	fadeall();
+      
+      // Wait a little bit before we loop around and do it again
+    	vTaskDelay(30 / portTICK_PERIOD_MS);
+    }
+    Serial.print("x");
+
+    // if(!(count % 20)) {
+    if(!(count % 20)) {
+      uxHighWaterMark = uxTaskGetStackHighWaterMark(NULL);
+      Serial.printf("\nvMainTaskAnimCylon: %i, ", count);
+      Serial.println(uxHighWaterMark);
+    }
+  count++;
+  }
+
+}
+
+
+void vMainTaskAnalogRead(void *pvParameters){  // This is a task.
+  (void) pvParameters;
+  int count = 0;
   
-    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
-      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
-    }
-    
-    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-    if( random8() < SPARKING ) {
-      int y = random8(7);
-      heat[y] = qadd8( heat[y], random8(160,255) );
-    }
-
-    // Step 4.  Map from heat cells to LED colors
-    for( int j = 0; j < NUM_LEDS; j++) {
-      CRGB color = HeatColor( heat[j]);
-      int pixelnumber;
-      if( gReverseDirection ) {
-        pixelnumber = (NUM_LEDS-1) - j;
-      } else {
-        pixelnumber = j;
-      }
-      leds[pixelnumber] = color;
-    }
-}
-
-
-void loop()
-{
-
-    Serial.println("loop(): knob.update()...");
-    knob.update();
-    
-    // TODO remove .5sec delay (just for prototyping)
-    delay(500);
-
-    Serial.print("knob.get(): ");
-    Serial.println(knob.get());
-
-
-    _radioData.OnTimeMillis = millis();
-
-    // enter SEND mode AT MOST every ~1 sec or so 
-    // (millis() will always not always be called w/ sub-ms frequency)
-    if (_radioData.OnTimeMillis % 1000 == 0)
-    {
-        String msg = "<== SENT [";
-        msg += RADIO_ID;
-        msg += "=>";
-        msg += SHARED_RADIO_ID;
-        msg += "]: ";
-        msg += _radioData.OnTimeMillis;
-        msg += " ms (";
-        msg += _radioData.FailedTxCount;
-        msg += " Failed TX)";
-        Serial.print(msg);
-        
-        // liteswarm#master/Radio.h#72
-        // _radio.send(SHARED_RADIO_ID, &_outboundRadioPacket, sizeof(_outboundRadioPacket), NRFLite::NO_ACK);
-        if (_radio.send(SHARED_RADIO_ID, &_radioData, sizeof(_radioData)), NRFLite::NO_ACK)
-        {
-            Serial.println("...Success");
-        }
-        else
-        {
-            Serial.println("...Failed");
-            _radioData.FailedTxCount++;
-        }
-    }
-
-    // SIDE-EFFECT: hasData() leaves radio in receive mode
-    while (_radio.hasData())
-    {
-        _radio.readData(&_radioData);
-        
-        String msg = "==> RCVD [  ";
-        msg += _radioData.SenderId;
-        msg += "=>";
-        msg += SHARED_RADIO_ID;
-        msg += "]: ";
-        msg += _radioData.OnTimeMillis;
-        msg += " ms (";
-        msg += _radioData.FailedTxCount;
-        msg += " Failed TX)";
-
-        Serial.println(msg);
-    }
-
-
-  // Add entropy to random number generator; we use a lot of it.
-  // random16_add_entropy( random());
-
-  if( millis() < 5000 )
-  {
-      Fire2012(); // run simulation frame
-  } else {
-    FastLED.clear( true );
+  Serial.println("started vMainTaskAnalogRead");
+  // Check if the given analog pin is usable - if not - delete this task
+  if(!adcAttachPin(ANALOG_INPUT_PIN)){
+    Serial.printf("TaskAnalogRead cannot work because the given pin %d cannot be used for ADC - the task will delete itself.\n", ANALOG_INPUT_PIN);
+    analog_read_task_handle = NULL; // Prevent calling vTaskDelete on non-existing task
+    vTaskDelete(NULL); // Delete this task
   }
   
-  
-  FastLED.show(); // display this frame
-  FastLED.delay(1000 / FRAMES_PER_SECOND);
+/*
+  AnalogReadSerial
+  Reads an analog input on pin A3, prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A3, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+*/
+
+  for (;;){
+    // read the input on analog pin:
+    int sensorValue = analogRead(ANALOG_INPUT_PIN);
+    // delay(100); // 100ms delay
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    count++;
+    Serial.print("vMainTaskAnalogRead: ");
+    Serial.print(count);
+    Serial.print(" - ");
+    Serial.println(sensorValue);
+    // if(count >= 10) vTaskDelete(NULL);
+  }
 }
